6e10f13d0bdbc2d1df8aba0132cc7854b1c0c55e
AISWARYA ANILKUMAR <aiswaryaanilkumar33@gmail.com>
2024-08-10 23:29:05 +0530

    Python Practise

---
+++ swap_num.py
@@ -0,0 +1,10 @@
+
+n1=int(input("Enter first number : "))
+n2=int(input("Enter second number : "))
+
+
+print("Before swap :"+str(n1)+", "+str(n2))
+temp=n1
+n1=n2
+n2=temp
+print("After swap :"+str(n1)+" ,"+str(n2))
